36b2b3c0e4d3edbeb373ebe6dd394e5f629cced9
baromeg <baromeg@gmail.com>
2020-10-15 09:49:37 +0100

    MVP ready

--- app.js
+++ app.js
@@ -18,20 +18,23 @@ const trunks = document.querySelector('.trunks')
 const trunks2 = document.querySelector('.trunks2')
 const trunks3 = document.querySelector('.trunks3')
 
-const trunksRight = [18, 19, 20, 21, 22, 23, 24, 25, 26]
+const trunksArray = [30, 29, 28, 24, 23, 22, 20, 19, 18]
+const waterWithoutTrunks = [35, 34, 33, 32, 31, 27, 26, 25, 21]
+
+const carsArrayRight = [53, 52, 51, 50, 49, 48, 47, 46, 45]
+const carsArrayLeft = [54, 55, 56, 57, 58, 59, 60, 61, 62]
+const crocodile = [9, 10, 11, 12, 13, 14, 15, 16, 17, 36, 37, 38, 39, 40, 41, 2, 43, 44, 63, 64, 65, 66, 67, 68, 69, 70, 71]
+
+// cells[18].classList.add('water')
 
 let frog = 76
-let lilyLeavePosition = 1
-let waterFront = 0
+const lilyLeavePosition = [1, 3, 5, 7]
+const waterFront = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 let carsPosition1 = 8
 let carsPosition2 = 0
 let carsPosition3 = 5
-let trunkPosition1 = 0
-let trunkPosition2 = trunkPosition1 + 1
-let trunkPosition3 = trunkPosition2 + 1
-let trunkPosition4 = trunkPosition3 + 1
-let trunkPosition5 = trunkPosition4 + 1
-let trunkPosition6 = trunkPosition5 + 1
+let trunkPosition1 = 18
+
 
 
 //* Creates the grid based on the width
@@ -49,40 +52,50 @@ cells[frog].classList.add('frog')
 //* Start of the game / reset frog position
 startButton.addEventListener('click', () => {
   startGame()
-
+  cells[frog].classList.remove('frog')
+  frog = 76
+  cells[frog].classList.add('frog')
 })
 
+
+
+waterFront.forEach((waterPosition) => {
+
+  cells[waterPosition].classList.add('water')
+
+
+
+})
 //* Initial position of the lilyleaves
-cells[lilyLeavePosition].classList.add('lilyLeave')
-cells[3].classList.add('lilyLeave')
-cells[5].classList.add('lilyLeave')
-cells[7].classList.add('lilyLeave')
+lilyLeavePosition.forEach((lily) => {
 
-cells[waterFront].classList.add('water')
-cells[2].classList.add('water')
-cells[4].classList.add('water')
-cells[6].classList.add('water')
-cells[8].classList.add('water')
+  cells[lily].classList.add('lilyLeave')
+  cells[lily].classList.remove('water')
+})
 
-console.log(cells)
+
 
 //* Creates the movement of the frog
 document.addEventListener('keydown', (event) => {
   const key = event.key
   console.log(key)
   if (key === 'ArrowUp' && !(frog < width)) {
+    cells[frog].classList.remove('frogWater')
     cells[frog].classList.remove('frog')
     frog -= width
     cells[frog].classList.add('frog')
   } else if (key === 'ArrowDown' && !(frog > (width ** 2) - width - 1)) {
+    cells[frog].classList.remove('frogWater')
     cells[frog].classList.remove('frog')
     frog += width
     cells[frog].classList.add('frog')
   } else if (key === 'ArrowLeft' && !(frog % width === 0)) {
+    cells[frog].classList.remove('frogWater')
     cells[frog].classList.remove('frog')
     frog -= 1
     cells[frog].classList.add('frog')
   } else if (key === 'ArrowRight' && !(frog % width === width - 1)) {
+    cells[frog].classList.remove('frogWater')
     cells[frog].classList.remove('frog')
     frog += 1
     cells[frog].classList.add('frog')
@@ -117,9 +130,6 @@ setInterval(() => {
     firstCarArray[carsPosition1].classList.remove('cars')
     carsPosition1 = 8
     firstCarArray[carsPosition1].classList.add('cars')
-  }
-  if (firstCarArray[carsPosition1].classList.contains('frog')) {
-    deadStopGame()
   }
   carCycle1()
 }, 800)
@@ -167,152 +177,97 @@ const carCycle3 = () => {
 
 //! Movement of the trunks
 // //*Created individual arrays for each car to loop through
-const firstTrunkArray = cells.slice(18, 27)
+// const firstTrunkArray = cells.slice(18, 27)
 
-for (let index = 0; index < firstTrunkArray.length; index++) {
-  firstTrunkArray[index].classList.add('water')
+// for (let index = 0; index < firstTrunkArray.length; index++) {
+//   firstTrunkArray[index].classList.add('water')
 
-}
+// }
 
-firstTrunkArray[trunkPosition1].classList.add('trunks')
-firstTrunkArray[trunkPosition2].classList.add('trunks2')
-firstTrunkArray[trunkPosition3].classList.add('trunks3')
+// firstTrunkArray[trunkPosition1].classList.add('trunks')
+// firstTrunkArray[trunkPosition2].classList.add('trunks2')
+// firstTrunkArray[trunkPosition3].classList.add('trunks3')
 
-const secondTrunkArray = cells.slice(27, 36)
+// const secondTrunkArray = cells.slice(27, 36)
 
-for (let index = 0; index < secondTrunkArray.length; index++) {
-  secondTrunkArray[index].classList.add('water')
+// for (let index = 0; index < secondTrunkArray.length; index++) {
+//   secondTrunkArray[index].classList.add('water')
 
-}
+// }
 
-secondTrunkArray[trunkPosition4].classList.add('trunks')
-secondTrunkArray[trunkPosition5].classList.add('trunks2')
-secondTrunkArray[trunkPosition6].classList.add('trunks3')
-
-
-
-// //* Each trunk has it's own interval to avoid conflicts in the loops
-setInterval(() => {
-  // console.log(trunkPosition1 === frog)
-  console.log(trunkPosition1)
-  console.log((frog - 18))
-  if ((frog - 18) === trunkPosition1) {
-
-    frogOnTrunk()
+// secondTrunkArray[trunkPosition4].classList.add('trunks')
+// secondTrunkArray[trunkPosition5].classList.add('trunks2')
+// secondTrunkArray[trunkPosition6].classList.add('trunks3')
 
 
 
-  }
-  if ((trunkPosition1 % width === width - 1)) {
-    firstTrunkArray[trunkPosition1].classList.remove('trunks')
-    trunkPosition1 = 0
-    firstTrunkArray[trunkPosition1].classList.add('trunks')
-  }
-
-  trunkCycle1()
-}, 400)
-
-const trunkInt1 = setInterval(() => {
-
-  if ((trunkPosition2 % width === width - 1)) {
-
-    firstTrunkArray[trunkPosition2].classList.remove('trunks2')
-    trunkPosition2 = 0
-    firstTrunkArray[trunkPosition2].classList.add('trunks2')
-  }
-  // if (firstTrunkArray[trunkPosition1].classList.contains('frog')) {
-  //   deadStopGame()
-  // }
-  trunkCycle2()
-}, 400)
-
-const trunkInt2 = setInterval(() => {
-
-  if ((trunkPosition3 % width === width - 1)) {
-
-    firstTrunkArray[trunkPosition3].classList.remove('trunks3')
-    trunkPosition3 = 0
-    firstTrunkArray[trunkPosition3].classList.add('trunks3')
-  }
-
-  trunkCycle3()
-}, 400)
-//* 2nd array of trunks
-const trunkInt3 = setInterval(() => {
-
-  if ((trunkPosition4 % width === width - 1)) {
-    secondTrunkArray[trunkPosition4].classList.remove('trunks')
-    trunkPosition4 = 0
-    secondTrunkArray[trunkPosition4].classList.add('trunks')
-  }
-  trunkCycle4()
-}, 700)
-
+// //* Intervals to create the movement of objects
 setInterval(() => {
 
-  if ((trunkPosition5 % width === width - 1)) {
+  trunksArray.forEach((element, i) => {
+    // element += index
+    if (frog === element) {
+      if (element === 35 || element === 26) {
+        cells[35].classList.remove('trunks')
+        trunksArray[i] = 18
+        cells[element].classList.remove('frogWater')
+        deadStopGame()
+      } else {
+        cells[element].classList.remove('frog')
+        cells[element].classList.remove('frogWater')
+        frog += 1
+        trunksArray[i] += 1
+        element += 1
+        cells[element].classList.add('frogWater')
+      }
+    } else if (element === 35) {
+      cells[35].classList.remove('trunks')
+      trunksArray[i] = 18
 
-    secondTrunkArray[trunkPosition5].classList.remove('trunks2')
-    trunkPosition5 = 0
-    secondTrunkArray[trunkPosition5].classList.add('trunks2')
-  }
-  trunkCycle5()
-}, 700)
+    } else if (firstCarArray[carsPosition1].classList.contains('frog')) {
+      deadStopGame()
+    } else if (cells[frog].classList.contains('lilyLeave')) {
+      displayScore.innerHTML += 50
+      confirm('You win! Do you want to play again?')
+      startGame()
+    } else if (cells[frog].classList.contains('water')) {
+      deadStopGame()
+    } else {
+      cells[element].classList.remove('trunks')
+      trunksArray[i] += 1
+      element += 1
+      cells[element].classList.add('trunks')
+    }
+  })
 
-setInterval(() => {
+  waterWithoutTrunks.forEach((element, i) => {
+    // element += index
+    if (frog === element) {
+      deadStopGame()
 
-  if ((trunkPosition6 % width === width - 1)) {
 
-    secondTrunkArray[trunkPosition6].classList.remove('trunks3')
-    trunkPosition6 = 0
-    secondTrunkArray[trunkPosition6].classList.add('trunks3')
-  }
-  trunkCycle6()
-}, 700)
+    } if (element === 35) {
+      cells[35].classList.remove('water')
+      waterWithoutTrunks[i] = 18
+      // else if (frog === 35) {
+      //   deadStopGame()
+      // } 
+
+
+    } else {
+      cells[element].classList.remove('water')
+      waterWithoutTrunks[i] += 1
+      element += 1
+      cells[element].classList.add('water')
+    }
+  })
+}, 400)
 
 
 
 // //*Each car has it's own function, as when I created a function which you could input (array, position) came up as undefined, I believe it was because of the -= operation
-const frogOnTrunk = () => {
-  firstTrunkArray[frog].classList.remove('frog')
-  firstTrunkArray[frog].classList.add('frogWater')
-  trunkPosition1 += 1
-  firstTrunkArray[frog].classList.add('frogWater')
-  firstTrunkArray[frog].classList.add('frog')
-}
-const trunkCycle1 = () => {
-  firstTrunkArray[trunkPosition1].classList.remove('trunks')
-  trunkPosition1 += 1
-  firstTrunkArray[trunkPosition1].classList.add('trunks')
 
-}
-const trunkCycle2 = () => {
-  firstTrunkArray[trunkPosition2].classList.remove('trunks2')
-  trunkPosition2 += 1
-  firstTrunkArray[trunkPosition2].classList.add('trunks2')
-}
-const trunkCycle3 = () => {
-  firstTrunkArray[trunkPosition3].classList.remove('trunks3')
-  trunkPosition3 += 1
-  firstTrunkArray[trunkPosition3].classList.add('trunks3')
-}
 
-const trunkCycle4 = () => {
-  secondTrunkArray[trunkPosition4].classList.remove('trunks')
-  trunkPosition4 += 1
-  secondTrunkArray[trunkPosition4].classList.add('trunks')
-
-}
-const trunkCycle5 = () => {
-  secondTrunkArray[trunkPosition5].classList.remove('trunks2')
-  trunkPosition5 += 1
-  secondTrunkArray[trunkPosition5].classList.add('trunks2')
-}
-const trunkCycle6 = () => {
-  secondTrunkArray[trunkPosition6].classList.remove('trunks3')
-  trunkPosition6 += 1
-  secondTrunkArray[trunkPosition6].classList.add('trunks3')
-}
 
 const startGame = () => {
   cells[frog].classList.remove('frog')
@@ -321,6 +276,20 @@ const startGame = () => {
   displayLives.innerHTML = 3
   displayScore.innerHTML = 0
 }
+
+//* Player wins if reaches the lilyleaves
+
+console.log(cells[frog] === cells[1])
+
+// || cells[frog] === cells[3] || cells[frog] === cells[5] || cells[frog] === cells[7]))
+// {
+//   confirm('You win! Do you want to play again?')
+//   displayScore.innerHTML += 50
+// } else if (cells[frog] === 1 || cells[frog] === 3 || cells[frog] === 5 || cells[frog] === 7) {
+//   deadStopGame()
+// }
+
+
 
 const deadStopGame = () => {
   if (displayLives.innerHTML <= 1) {
